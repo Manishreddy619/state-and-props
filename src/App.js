069f8ff89604c7cc76f147502b5ce845c84dd826
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-08-25 19:22:57 +0200

    search query added

--- src/App.js
+++ src/App.js
@@ -3,27 +3,18 @@ import MyBadge from './components/MyBadge';
 import WarningSign from './components/WarningSign';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SingleBook from './components/SingleBook';
-
+import Book from '../src/components/data.json';
 import BookComponent from './components/BookComponent';
 import FiterBook from './components/FiterBook';
 
 function App() {
-	const book = [
-		{
-			img: 'https://images-eu.ssl-images-amazon.com/images/I/51Gw57YKuFL._SX198_BO1,204,203,200_QL40_ML2_.jpg',
-			price: '23.99',
-			title: 'Mamba Mentality',
-			category: 'Insipiration',
-			asin: '834709',
-		},
-	];
 	return (
 		<div className='App'>
 			<WarningSign text='Danger here' />
-			<FiterBook />
+
 			<MyBadge color='primary' text='Give me Some sunshine' />
-			<SingleBook myObj={book} />
-			<BookComponent />
+			<SingleBook item={Book[0]} />
+			<BookComponent item={Book} />
 		</div>
 	);
 }
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,52 +1,47 @@
 import React from 'react';
 import './SingleBook.css';
 import { Col, Container, Row } from 'react-bootstrap';
-const SingleBook = ({ myObj }) => {
-	console.log(myObj);
-	return (
-		<Container>
-			<h2>HistoryBooks</h2>
-			<Row>
-				{myObj.map((book) => {
-					return (
-						<Col
-							xs={12}
-							sm={6}
-							md={4}
-							lg={3}
-							className='mt-3'
-							id='main'
-							style={{ marginBottom: '3rem' }}
-							key={myObj.asin}>
-							<div className='card text-truncate'>
-								<img className='image cover' src={book.img} />
-								<div className='book-info'>
-									<div className='title'>Title: {book.title}</div>
-									<div className='byline'>Category: {book.category}</div>
-									<div className='rating'>
-										<span className='stars'></span>
-										<span className='votes'>{book.price} $</span>
-									</div>
+import { Component } from 'react';
+class SingleBook extends Component {
+	render() {
+		return (
+			<Container>
+				<Row>
+					<Col
+						className='mt-3'
+						id='main'
+						style={{ marginBottom: '3rem' }}
+						key={this.props.item.asin}>
+						<div className='card text-truncate'>
+							<img className='image cover' src={this.props.item.img} />
+							<div className='book-info'>
+								<div className='title'>Title: {this.props.item.title}</div>
+								<div className='byline'>
+									Category: {this.props.item.category}
 								</div>
-								<div className='likes'>
-									<span className='image-container'>
-										<img
-											className='image person'
-											src='https://images.pexels.com/photos/157661/young-woman-shooting-model-157661.jpeg?h=350&amp;auto=compress&amp;cs=tinysrgb'
-										/>
-									</span>
-									<span className='people'>
-										<span className='name'>Isabella Elliott</span>
-										&nbsp;likes this
-									</span>
+								<div className='rating'>
+									<span className='stars'></span>
+									<span className='votes'>{this.props.item.price} $</span>
 								</div>
 							</div>
-						</Col>
-					);
-				})}
-			</Row>
-		</Container>
-	);
-};
+							<div className='likes'>
+								<span className='image-container'>
+									<img
+										className='image person'
+										src='https://images.pexels.com/photos/157661/young-woman-shooting-model-157661.jpeg?h=350&amp;auto=compress&amp;cs=tinysrgb'
+									/>
+								</span>
+								<span className='people'>
+									<span className='name'>Isabella Elliott</span>
+									&nbsp;likes this
+								</span>
+							</div>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
+}
 
 export default SingleBook;
